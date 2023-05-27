943dbbd98578e1c5bb7283e421525af379a67c11
isel-har <ismailhrb1337@gmail.com>
2023-05-27 23:11:52 +0100

    walls

--- src/creation.c
+++ src/creation.c
@@ -1,6 +1,6 @@
 #include "../inc/cub.h"
 
-void	draw_vector(t_cub *cub)
+void draw_vector(t_cub *cub)
 {
 	cub->next_x = cub->p_x + cos(cub->angle) * 50;
 	cub->next_y = cub->p_y + sin(cub->angle) * 50;
@@ -8,10 +8,10 @@ void	draw_vector(t_cub *cub)
 	mlx_pixel_put(cub->m_ptr, cub->w_ptr, cub->next_x, cub->next_y, 0xff0000);
 }
 
-void	put_square(t_cub *cub, int x, int y)
+void put_square(t_cub *cub, int x, int y)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	j = 0;
 	while (j < 32)
@@ -24,15 +24,14 @@ void	put_square(t_cub *cub, int x, int y)
 		}
 		j += 1;
 	}
-
 }
 
-void	draw_mini_map(t_cub *cub)
+void draw_mini_map(t_cub *cub)
 {
-	int	x;
-	int	y;
-	int	cofx;
-	int	cofy;
+	int x;
+	int y;
+	int cofx;
+	int cofy;
 
 	y = 0;
 	while (cub->map_2d[y])
@@ -53,8 +52,7 @@ void	draw_mini_map(t_cub *cub)
 	}
 }
 
-
-void	orient_player(t_cub *cub, int color)
+void orient_player(t_cub *cub, int color)
 {
 	/*int	j;
 	int	i;
@@ -81,9 +79,26 @@ void	orient_player(t_cub *cub, int color)
 		tmp += 1;
 	}*/
 }
-void	move_player(t_cub *cub, char o)
+
+bool	check_wall(t_cub *cub)
 {
-	orient_player(cub, 0x000000);
+	int	fpx;
+	int	fpy;
+
+	fpx = (cub->p_x) / 32;
+	fpy = (cub->p_y) / 32;
+	if (cub->map_2d[fpy][fpx] == '1')
+		return (true);
+	return (false);
+}
+
+void move_player(t_cub *cub, char o)
+{
+	int	xtmp;
+	int	ytmp;
+
+	xtmp = cub->p_x;
+	ytmp = cub->p_y;
 	if (o == 'u')
 		cub->p_y -= 1;
 	if (o == 'd')
@@ -92,23 +107,31 @@ void	move_player(t_cub *cub, char o)
 		cub->p_x -= 1;
 	if (o == 'r')
 		cub->p_x += 1;
+	if (check_wall(cub))
+	{
+		printf("dkhl \n");
+		cub->p_x = xtmp;
+		cub->p_y = ytmp;
+	}
+	else
+		mlx_pixel_put(cub->m_ptr, cub->w_ptr, xtmp, ytmp, 0x00000);
 }
 
-int	ft_move(int keycode, t_cub *cub)
+int ft_move(int keycode, t_cub *cub)
 {
-	if (keycode  == 13)
+	if (keycode == 13)
 		move_player(cub, 'u');
-	if (keycode  == 1)
+	if (keycode == 1)
 		move_player(cub, 'd');
-	if (keycode  == 0)
+	if (keycode == 0)
 		move_player(cub, 'l');
-	if (keycode  == 2)
+	if (keycode == 2)
 		move_player(cub, 'r');
-	if (keycode  == 124)
+	if (keycode == 124)
 	{
 		cub->angle += 0.0872665;
 	}
-	if (keycode  == 123)
+	if (keycode == 123)
 	{
 		cub->angle -= 0.0872665;
 	}
@@ -119,7 +142,7 @@ int	ft_move(int keycode, t_cub *cub)
 	return (0);
 }
 
-void	put_player(t_cub *cub)
+void put_player(t_cub *cub)
 {
 	cub->p_x *= 32;
 	cub->p_y *= 32;
@@ -127,39 +150,39 @@ void	put_player(t_cub *cub)
 	cub->p_y += 32 / 2;
 	orient_player(cub, 0xff0000);
 	draw_vector(cub);
-	mlx_hook(cub->w_ptr, 02, (1L<<0), ft_move, cub);
+	mlx_hook(cub->w_ptr, 02, (1L << 0), ft_move, cub);
 }
 
-void	get_angle(t_cub *cub)
+void get_angle(t_cub *cub)
 {
 	if (cub->p_or == NORTH)
 		cub->angle = 270 * (M_PI / 180);
 	if (cub->p_or == SOUTH)
 		cub->angle = 90 * (M_PI / 180);
 	if (cub->p_or == EAST)
-		cub->angle = 0,00;
+		cub->angle = 0, 00;
 	if (cub->p_or == WEST)
 		cub->angle = 180 * (M_PI / 180);
 }
 
-void	creation(t_cub *cub)
+void creation(t_cub *cub)
 {
 	cub->angle = 0.00;
 	cub->y_map = 0;
 	cub->x_map = 0;
-	int	tmp = 0;
-	while (cub->map_2d[cub->y_map ])
+	int tmp = 0;
+	while (cub->map_2d[cub->y_map])
 	{
 		cub->x_map = 0;
 		while (cub->map_2d[cub->y_map][cub->x_map])
 			cub->x_map += 1;
 		if (cub->x_map > tmp)
 			tmp = cub->x_map;
-		cub->y_map  += 1;
+		cub->y_map += 1;
 	}
-	cub->x_map  = tmp;
+	cub->x_map = tmp;
 	cub->m_ptr = mlx_init();
-	cub->w_ptr = mlx_new_window(cub->m_ptr, cub->x_map  * 32, cub->y_map  * 40, "cub3d");
+	cub->w_ptr = mlx_new_window(cub->m_ptr, cub->x_map * 32, cub->y_map * 40, "cub3d");
 	get_angle(cub);
 	draw_mini_map(cub);
 	put_player(cub);
